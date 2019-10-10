c763604457bb45c45ac74fecc9d57965754613fe
Nick <nick@rhinodispatch.com>
2019-10-10 13:35:59 +0300

    different approach to test updating

--- test/updating_test.js
+++ test/updating_test.js
@@ -12,8 +12,9 @@ describe('updating records', () => {
 
     it('updated the user in the db', async () => {
         const props = { name: 'John' };
-        const result = await User.updateOne({ name: user.name }, props);
-        assert(result.nModified === 1);
+        const result = await User.updateOne({ name: user.name }, { $set: props });
+        const person = await User.findById(user._id);
+        assert(person.name === props.name);
     });
 
 });
